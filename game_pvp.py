7110b948779e749e00d8d61866816e6ff5e35dd4
Benedek Novak <benedeknovak@gmail.com>
2023-05-20 12:24:32 +0200

    game.move() removed

--- game_pvp.py
+++ game_pvp.py
@@ -2,7 +2,7 @@ from checkers_graphics import *
 from random import randint
 from player_movement import MovingPiece
 
-def startcol(place):
+def startcol(place): ## A function to determine the color of each piece when initializing the board
     return (place[0]+place[1])%2*(-1) if place[0]>4 else (place[0]+place[1])%2
 
 def is_valid(pos, board, start): ## a place is valid if its not occupied or off the board
@@ -11,10 +11,18 @@ def is_valid(pos, board, start): ## a place is valid if its not occupied or off 
     return False
 
 class Piece():
+    '''
+    Used for the pieces on the board.
+    Its attributes are listed and described under self.__init__()
+    Its main function is self.update that updates these attributes
+    The other functions are used as subroutines of this self.update
+    '''
+
     def __init__(self, GFX, pos, col, crowned = False):
         self.col = col # color
         self.options = [] # available positions to move to
-        self.det_opt = dict() # detailed options: {newpos:[value*, [pieces took], got_crowned, [path]]} * = #pieces took + I_{just got crowned} + 100*I_{won the game}
+        self.det_opt = dict() # detailed options: {newpos:[value, [pieces took], got_crowned, [path]]} 
+        # value = #pieces took + 1 if it crowns the piece + 100 if the move wins the game
         self.crowned = crowned # if the piece is crowned
         self.can_take = False # can the piece take
         self.pos = pos # current position
@@ -31,6 +39,9 @@ class Piece():
     #     self.piece_gfx.remove_from(self.GFX)
 
     def __str__(self):
+        '''
+        The gamestate is also printed in the terminal when running the game
+        '''
         if not self.crowned:
             return " 1" if self.col==1 else "-1"
         else:
@@ -40,7 +51,7 @@ class Piece():
         '''
         Updates the following attributes of the piece:
         self.options (where can the piece move)
-        self.det_opt (what pieces get taken for each move)
+        self.det_opt (what pieces get taken for each move, what positions are visited, the value of the move, and if the piece got crowned while moving)
         self.can_take (can the piece take)
         '''
         self.options = []
@@ -59,16 +70,16 @@ class Piece():
             for direction in [(self.col, 1), (self.col, -1)]: ## checks if the piece can take in the right direction
                 self.check_take(board, direction, self.pos, [(self.col, 1), (self.col, -1)], self.pos)
 
-            ## Uteskenyszer miatt
+            ## it adds its neighbors only if it can't take
             if not self.can_take:
                 for direction in [(self.col, 1), (self.col, -1)]: ## checks if the piece can take in the right direction
                     self.no_take(board, direction)
 
-        for opt in self.det_opt.keys():
+        for opt in self.det_opt.keys(): ## adds all the options to the self.options list
             self.options.append(opt)
 
         self.crowned_bonus() ## gives bonus points for crowning a piece
-        self.game_end_bonus(board)
+        self.game_end_bonus(board) ## gives bonus points for winning the game
 
 
     def check_take(self, board, direction, pos, dirs, start, already_taken = []):
@@ -86,7 +97,7 @@ class Piece():
         if check_pos in already_taken: ## it cant take the same piece twice 
             return set() # (and it also cant make a normal move after taking)
 
-    ## checks if the piece can take in the given direction in a given board state
+        ## checks if the piece can take in the given direction in a given board state
         if check_pos in board.keys(): #if there is a piece in that direction
             if (board[check_pos].col != self.col # and its an enemy
             and is_valid((check_pos[0]+direction[0], check_pos[1]+direction[1]), board.keys(), start)): # and its on the board
@@ -129,28 +140,24 @@ class Piece():
 
     def no_take(self,board, direction): 
         '''
-        Lists all the free positions in the given direction and adds them to self.det_opt
-        Only called if the piece can't take, so there is no need to check any further
+        Lists all the free positions in the given direction and adds them to self.det_opt.
+        Only called if the piece can't take.
         '''
-        ## used to list options if the piece cant take
-        ## adds the position in the given direction to options if not occupied
         newpos = (self.pos[0]+direction[0], self.pos[1]+direction[1]) ## position to check
-        if is_valid(newpos, board.keys(), (9,9)): ## if its not occupied or off the board; (9,9) because: since it didn't take it cant move back to the original position
+        if is_valid(newpos, board.keys(), (9,9)): ## if its not occupied or off the board; (9,9) because there is no need to check for circles in this case
             self.det_opt[newpos] = [0, [], False]
             self.det_opt[newpos][2] = ((newpos[0]==0 and self.col == -1) or (newpos[0]==7 and self.col == 1))
 
     def can_take_more(self,board,pos,dirs,start, already_taken = []):
         '''
-        Checks if the piece can take someting from [pos] position given [board] boardstate in one of the [dirs] directions except for [prev_dir]
-        This is used when checking for sequential takes, so the pos argument is not necessarily the same as self.pos
-        The piece can only take pieces that it didn't take before. - this is assured with already_taken
+        Checks if the piece can take someting from [pos] position given [board] boardstate in one of the [dirs], but it can't take pieces in [already_taken], because they were taken in one of the previous steps.
+        This is used when checking for sequential takes, so the pos argument is not necessarily the same as self.pos, which is the starting point.
         '''
-         ## can the piece take sg? - used for the depth search
-        for direction in dirs:
+        for direction in dirs: #checks every direction
             check_pos = (pos[0]+direction[0], pos[1]+direction[1])
             if check_pos not in already_taken and check_pos in board.keys(): #if there is a piece in that direction that we didnt take yet
                 if (board[check_pos].col != self.col # and its an enemy
-                and is_valid((check_pos[0]+direction[0], check_pos[1]+direction[1]), board.keys(), start)): ## its a valid place
+                and is_valid((check_pos[0]+direction[0], check_pos[1]+direction[1]), board.keys(), start)): ## and the next place is a valid place to go to (not occupied or off the board)
                     return True
                     
         return False
@@ -173,18 +180,24 @@ class Piece():
                 self.det_opt[newpos][0]+=100
 
 class game():
+    '''
+    The main object of the game.
+    Stores the board in a dictionary with the position of each piece as its key and the piece object as the value
+    The other variables are decribed under self.__init__()
+    '''
+
     def __init__(self, mode='bvb'):
         self.GFX = CheckersGraphics()
         self.board = {pos: Piece(self.GFX, pos, startcol(pos))
                        for pos in [(i,j) for i in range(8) for j in range(8) if (i+j)%2==1 and (i<3 or i>4)]}
-        self.turn = 1
-        self.can_move = []
-        self.mode = mode
+        self.turn = 1 ## values: -1, 1: which players turn is it
+        self.can_move = [] ## pieces that can move in the current turn
+        self.mode = mode ## the gamemode: player vs player/ player vs bot/ bot vs bot
 
-        self.game_state = 1
+        self.game_state = 1 ## values: 1 if the game is running, 0 if it is over
         self.update_all()
 
-    def __str__(self): # its ugly af but it works
+    def __str__(self): # prints the board in the terminal
         return "\n".join([str([str(self.board[(i,j)]) if (i,j) in self.board.keys() else " 0" for j in range(8)]).replace("'", "").replace(",", "")
                           for i in range(8)])
     
@@ -210,7 +223,7 @@ class game():
         piece.det_opt
         piece.can_take
 
-        Then lists all of them that can move to self.can_move
+        Then lists all of those that can move into self.can_move
         '''
         for p in self.board.values():
             p.update(self.board)
@@ -244,13 +257,13 @@ class game():
 
     def step(self, pos, newpos):
         '''
-        The selected piece (pos) is moved to newpos and all the taken pieces get removed
-        The piece also gets crowned if it reached the last row in the process
+        The selected piece (pos) is moved to newpos and all the taken pieces get removed.
+        The piece also gets crowned if it reached the last row in the process.
 
         Then changes the turn to the other player and updates the pieces that can move
         '''
-        ## taking an enemy piece
 
+        ## the part responsibe for the graphics
         piece_gfx = self.board[pos].piece_gfx
         if (len(self.board[pos].det_opt[newpos][1]) == 0):
             motion = GFXMotion(piece_gfx, pos, newpos)
@@ -277,7 +290,7 @@ class game():
                 self.board[newpos] = self.board[pos] ### we move our piece in these 3 steps:
                 self.board[newpos].pos = newpos###
                 del self.board[pos]###
-            self.board[newpos].update(self.board) # we check if the piece can take another (it will be done elsewhere)
+            self.board[newpos].update(self.board) # we check if the piece can take another 
 
         ## not taking an enemy piece
         else:
@@ -312,7 +325,7 @@ class game():
         and return the value (maximum points collected by the bot) - (maximum points collected by the player)
 
         depth = 1 means that the player takes one step after the bot
-        depth = n means that the player takes n moves and the bot n-1 other moves, because this move counts as the last one
+        depth = n means that the player takes n moves and the bot n-1 other moves after this
         '''
         newgame = self.simulate_step(pos, newpos) # the bot took its step, now its the players turn in this simulation
         if newgame.can_move == []: ## if the player cant make another move
@@ -363,7 +376,10 @@ class game():
         
         return best_list[c][0], best_list[c][1]
 
-    def player_turn(self): ## this function takes input from the user and makes changes to the board based on it
+    def player_turn(self):
+        '''
+        this function takes input from the user and makes changes to the board based on it while making the necessary changes on the UI
+        '''
         pos = '*'
         if len(self.can_move) == 0:
             print(f"GAME OVER \n {self.turn*(-1)} WINS")
@@ -378,7 +394,6 @@ class game():
         if piece.can_take: forced_capture = True
         else: forced_capture = False
         
-        # print(f"pieces to move: {self.can_move}")
         while True:
             pos = (-1,-1)
             self.GFX.possible_choices = self.can_move
@@ -486,6 +501,10 @@ class game():
         return 1
 
     def bot_turn(self):
+        '''
+        This function searches for the best step to take and the moves the bot accordingly
+        '''
+
         pos = '*'
         if len(self.can_move) == 0:
             print(f"GAME OVER \n {self.turn*(-1)} WINS")
@@ -500,26 +519,26 @@ class game():
         self.step(pos, newpos) # the player takes their step
         return 1
 
-    def move(self):
-        print(f"Current player: {self.turn}")
-        if len(self.can_move) == 0:
-            self.game_state = 0
-            print(f"GAME OVER \n {self.turn*(-1)} WINS")
-            return
+    # def move(self):
+    #     print(f"Current player: {self.turn}")
+    #     if len(self.can_move) == 0:
+    #         self.game_state = 0
+    #         print(f"GAME OVER \n {self.turn*(-1)} WINS")
+    #         return
 
-        if self.mode == 'pvp':
-            self.game_state = self.player_turn()
+    #     if self.mode == 'pvp':
+    #         self.game_state = self.player_turn()
 
-        elif self.mode == 'pvb':
-            print(f"Current player: {self.turn}")
-            if self.turn == 1:
-                self.game_state = self.player_turn()
-            elif self.turn == -1:
-                self.game_state = self.bot_turn()
+    #     elif self.mode == 'pvb':
+    #         print(f"Current player: {self.turn}")
+    #         if self.turn == 1:
+    #             self.game_state = self.player_turn()
+    #         elif self.turn == -1:
+    #             self.game_state = self.bot_turn()
         
-        elif self.mode == 'bvb':
-            print(f"Current player: {self.turn}")
-            self.game_state = self.bot_turn()
+    #     elif self.mode == 'bvb':
+    #         print(f"Current player: {self.turn}")
+    #         self.game_state = self.bot_turn()
 
     # def game_start_pvp(self): ## player vs player game
     #     game_state = 1
